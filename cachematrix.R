2fffb0b22bae490a2dc8bfe6bcd101db54d33e62
James Nguyen <triet280893@gmail.com>
2015-07-23 19:46:49 -0700

    completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates a structure that holds  a matrix and 
+## its inverse. It has 4 interfacing functions: set(), get(), 
+## setinverse(), and getinverse(). 
+##
+## set() and get() interact with the matrix while setinverse() 
+## and getinverse() interact with the matrix's inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        i <- NULL #inverse matrix
+        
+        set <- function(y) {
+                x <<- y
+                i < NULL
+        }
+        
+        get <- function () x
+        setinverse <- function (y) i <<- y
+        getinverse <- function () i
+        
+        list (set = set, get = get, #return interface
+              setinverse = setinverse, 
+              getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calculates and returns a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        i <- x$getinverse()
+        
+        if (!is.null(i)) return(i) #return i if value is already calculated
+        
+        data <- x$get() 
+        i <- solve(data, ...) #calculate inverse
+        
+        x$setinverse(i)
+        i
 }
+
+###################################################################
